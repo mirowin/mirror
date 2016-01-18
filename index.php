72eed592a9fcb5518f4c1a19983fd3a315258ce8
mirowin <shitikov.n@gmail.com>
2016-01-18 12:23:35 +0300

    change news to yandex

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 	<div class="top right"><div class="windsun small dimmed"></div><div class="temp"></div><div class="forecast small dimmed"></div></div>
 	<div class="center-ver center-hor"><!-- <div class="dishwasher light">Vaatwasser is klaar!</div> --></div>
 	<div class="lower-third center-hor"><div class="compliment light"></div></div>
-	<!-- <div class="bottom center-hor"><div class="news medium"></div></div> -->
+	<div class="bottom center-hor"><div class="news medium"></div></div>
 
 </div>
 
@@ -32,7 +32,7 @@
 <script src="js/compliments/compliments.js" type="text/javascript"></script>
 <script src="js/weather/weather.js" type="text/javascript"></script>
 <script src="js/time/time.js" type="text/javascript"></script>
-<!-- <script src="js/news/news.js" type="text/javascript"></script> -->
+<script src="js/news/news.js" type="text/javascript"></script>
 <script src="js/main.js?nocache=<?php echo md5(microtime()) ?>"></script>
 <!-- <script src="js/socket.io.min.js"></script> -->
 

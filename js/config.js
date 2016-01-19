74b50d8ff8a025be832b16fa83c0d026b7190f9f
mirowin <shitikov.n@gmail.com>
2016-01-19 10:59:55 +0300

    add new simple calendar

--- js/config.js
+++ js/config.js
@@ -35,9 +35,9 @@ var config = {
     },
     calendar: {
         maximumEntries: 10,
-        url: "https://p01-calendarws.icloud.com/ca/subscribe/1/n6x7Farxpt7m9S8bHg1TGArSj7J6kanm_2KEoJPL5YIAk3y70FpRo4GyWwO-6QfHSY5mXtHcRGVxYZUf7U3HPDOTG5x0qYnno1Zr_VuKH2M"
+        url: "https://calendar.google.com/calendar/embed?src=shitikov.n%40gmail.com&ctz=Europe/Moscow"
     },
     news: {
-        feed: 'https://news.yandex.ru/index.rss'
+        feed: 'https://news.yandex.ru/hardware.rss'
     }
 }
